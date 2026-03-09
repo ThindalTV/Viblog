10274ae80aa40a42bb2b0a7e252020d255ec673a
ThindalTV <thindaltv@gmail.com>
2026-03-09 17:39:56 +0100

    Removing frontend from viblog project.

--- Viblog/Frontend/Infrastructure/ShowProfileHeaderAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace Viblog.Frontend.Infrastructure;
-
-/// <summary>
-/// Attribute to indicate that a page should display the profile header
-/// </summary>
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-public sealed class ShowProfileHeaderAttribute : Attribute
-{
-}
--- Viblog/Frontend/Models/SocialLink.cs
+++
@@ -1,9 +0,0 @@
-namespace Viblog.Frontend.Models;
-
-/// <summary>
-/// Represents a social media link with URL, label, and icon
-/// </summary>
-/// <param name="Url">The URL of the social media profile</param>
-/// <param name="Label">The accessible label for the link</param>
-/// <param name="Icon">SVG icon markup</param>
-public record SocialLink(string Url, string Label, string Icon);
